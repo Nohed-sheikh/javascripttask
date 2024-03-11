5d96bc4b8076635087ffad71b53518381bb0ea35
Nohed sheikh <118043181+Nohed-sheikh@users.noreply.github.com>
2024-03-12 00:00:12 +0530

    Add files via upload

---
+++ drawingapp.js
@@ -0,0 +1,54 @@
+window.addEventListener('load',function(){
+    const canva = document.querySelector('#canvas');
+    const ctx = canva.getContext('2d')
+    //resizing
+    canva.height = window.innerHeight;
+    canva.width = window.innerWidth;
+
+
+    // ctx.fillRect(150,50,200,500);
+    /*
+    ctx.strokeStyle = "red";
+    ctx.lineWidth = 10;
+    ctx.strokeRect(150,50,200,500);
+    */
+
+    /*
+    ctx.beginPath();
+    ctx.moveTo(100,100);
+    ctx.lineTo(200,100);
+    ctx.lineTo(200,150);
+    ctx.closePath();
+    ctx.stroke()
+    */
+
+    //variables
+    let painting = false;
+    
+    function startPosition(e){
+       painting = true;
+       draw(e)
+    }
+
+    function finishPosition(){
+        painting = false;
+        ctx.beginPath();
+    }
+
+    function draw(e){
+       if(!painting) return;
+       ctx.linWidth = 10;
+       ctx.lineCap = "round"
+       ctx.strokeStyle = 'red'
+
+       ctx.lineTo(e.clientX, e.clientY);
+       ctx.stroke();
+       ctx.beginPath();
+       ctx.moveTo(e.clientX, e.clientY);
+    }
+    // Eventlistners
+    canva.addEventListener('mousedown',startPosition);
+    canva.addEventListener('mouseup',finishPosition);
+    canva.addEventListener('mousemove',draw);
+
+})
